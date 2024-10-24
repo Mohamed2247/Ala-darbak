7ad9d82406ad5498002618bc702d83c9f0bc5cd2
Mohamed Hamdy <139185513+Mohamed2247@users.noreply.github.com>
2024-10-25 00:03:52 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,22 @@ function validatePhoneNumber(input) {
   if (!input.value.startsWith("+966")) {
     input.value = "+966";
   }
-  input.value = input.value.replace(/\D/g, "").replace(/^966/, "+966");
 
-  if (input.value.length > 13) {
-    input.value = input.value.slice(0, 13);
+  let phoneNumber = input.value.replace(/\D/g, "").replace(/^966/, "");
+
+  let formattedNumber = "+966";
+
+  if (phoneNumber.length > 0) {
+    formattedNumber += " " + phoneNumber.substring(0, 2);
   }
+  if (phoneNumber.length >= 3) {
+    formattedNumber += " " + phoneNumber.substring(2, 5);
+  }
+  if (phoneNumber.length >= 6) {
+    formattedNumber += " " + phoneNumber.substring(5, 9);
+  }
+
+  input.value = formattedNumber.slice(0, 16);
 }
 
 $(document).ready(function () {
